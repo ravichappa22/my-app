8c7c896598254ac9c791aea8b320fd1abdcca080
ravchappa22 <chapparavi22@gmail.com>
2018-01-01 13:55:28 -0500

    added Error Handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { myErrorHandler } from './myErrorHandler';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler,
+   useClass: myErrorHandler}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
